9fcf88ae621d2b54ae45b3ede52891c8797d05c4
Leonardo <leonardofierens@gmail.com>
2023-09-20 10:04:55 -0300

    Base del proyecto

---
+++ src/main/java/utn/t2/s1/gestionsocios/GestionSociosApplication.java
@@ -0,0 +1,13 @@
+package utn.t2.s1.gestionsocios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionSociosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionSociosApplication.class, args);
+	}
+
+}
